69b0ff51f1cd0cc879ba1efc238d5a452c93fe85
rimeleaf <rimeleaf@sina.com>
2020-04-16 16:52:26 +0800

    auth

---
+++ cloud/gateway-sentinel/src/main/java/com/own/GatewaySentinelApplication.java
@@ -0,0 +1,18 @@
+package com.own;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author MECHREV
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GatewaySentinelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewaySentinelApplication.class, args);
+	}
+
+}
---
+++ cloud/order-sentinel/src/test/java/com/own/OrderSentinelApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.own;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class OrderSentinelApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
